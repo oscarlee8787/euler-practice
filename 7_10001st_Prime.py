02ce9a01eb8c6bc94d99605df15e650aaa54bf6e
Oscar Lee <oscarlee8787@gmail.com>
2023-12-25 09:56:22 +0100

    done

--- 7_10001st_Prime.py
+++ 7_10001st_Prime.py
@@ -5,17 +5,17 @@ What is the 10001st prime number?
 def prime():
     '''for every number i, if 2 till i**0.5 != 0, i is a prime'''
     prime_list = []
-    for i in range(1,3000):
+    for i in range(2,300000):
         count = 0
         for j in range(1, int(i**0.5)+1):
             if i%j == 0:
                 count += 1
         if count == 1:
             prime_list.append(i)
-        if len(prime_list) >= 10:
+        if len(prime_list) >= 10001:
             break
 
-    print(prime_list)
+    print(prime_list[10000])
     return
 
 prime()
