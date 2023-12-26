4990731bab52e564efb758342a790436eb88b1ca
Oscar Lee <oscarlee8787@gmail.com>
2023-12-26 18:22:33 +0100

    done

--- 10_Summation_of_Primes.py
+++ 10_Summation_of_Primes.py
@@ -9,16 +9,17 @@ def primess():
     returns the sum of the list
     '''
     list_ = []
-    for i in range(1, 200):
+    for i in range(2, 2000000): # finding primes from 1 - 200
         count = 0
-        for j in range(1, int(i**0.5)+1):
+        for j in range(1, int(i**0.5)+1): # if i is divisable by numbers from 1 to i^0.5
             if i%j == 0:
                 count += 1
-            if count > 1:
-                break
-            list_.append(i)
+                if count > 1:
+                    break # if more than 1 number can be divided without residual, i is not a prime
+        if count == 1:
+            list_.append(i) # add i to list of primes if count ==1 at end of prime test
 
-    print(list_)
+    print(sum(list_))
     return
 
 primess()
