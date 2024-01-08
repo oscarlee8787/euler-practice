0b71ea2bf3c1ae0b83299a45ba3f2e87f63895d4
Oscar Lee <oscarlee8787@gmail.com>
2024-01-08 10:28:44 +0800

    created num word dict

--- 17_Number_Letter_Counts.py
+++ 17_Number_Letter_Counts.py
@@ -5,3 +5,26 @@ how many letters would be used?
 NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two)
 contains 23 letters and 115 (one hundred and fifteen) contains 20 letters.
 The use of "and" when writing out numbers is in compliance with British usage.'''
+
+num_word_dict = {1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six',7:'seven',
+8:'eight',
+9:'nine',
+10:'ten',
+11:'eleven',
+12:'twelve',
+13:'thirteen',
+14:'fourteen',
+15:'fifteen',
+16:'sixteen',
+17:'seventeen',
+18:'eighteen',
+19:'nineteen',
+20:'twenty',
+30:'thrity',
+40:'forty',
+50:'fifty',
+60:'sixty',
+70:'seventy',
+80:'eighty',
+90:'ninety',
+100:'hundred'}
