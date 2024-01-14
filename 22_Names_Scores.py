4fc84c33bd788f57c16364e1548288f35e68b516
Oscar Lee <oscarlee8787@gmail.com>
2024-01-14 15:40:53 +0800

    p22 done

--- 22_Names_Scores.py
+++ 22_Names_Scores.py
@@ -6,3 +6,19 @@ which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list.
 So, COLIN would obtain a score of 938 * 53 = 49714.
 What is the total of all the name scores in the file?
 '''
+names = open('resources/names.txt').read().replace('"','').split(sep=',')
+names.sort()
+
+def name_score(word):
+    score = 0
+    for w in word:
+        score += ord(w)-64
+    return score
+
+total = 0
+
+for index, name in enumerate(names):
+    score = name_score(name)
+    total += (index+1) * score
+
+print(total)
--- 23_Non-Abundant_Sums.py
+++ 23_Non-Abundant_Sums.py
@@ -9,5 +9,26 @@ the smallest number that can be written as the sum of two abundant numbers is 24
 By mathematical analysis, it can be shown that all integers greater than 28123 can be written
 as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis
 even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+
 Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
 '''
+# find a list of abundant numbers smaller than 28123
+# create all combinations of sums of elements in list
+# save to another list
+
+def divisor_sum(num):
+    divisor_list = []
+    for i in range(1, int(num/2+1)):
+        if num%i == 0:
+            divisor_list.append(i)
+
+    return sum(divisor_list)
+
+# print(divisor_sum(12))
+
+abund_list = []
+for i in range(1, 14064):
+    if divisor_sum(i) > i:
+        abund_list.append(i)
+
+print(abund_list)
