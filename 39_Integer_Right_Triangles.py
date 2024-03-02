3bc473347601bc0059d307f830ff8345808e1ad8
Oscar Lee <oscarlee8787@gmail.com>
2024-03-02 21:00:22 +0800

    p40 done

--- 39_Integer_Right_Triangles.py
+++ 39_Integer_Right_Triangles.py
@@ -3,13 +3,30 @@
 {20,48,52}, {24,45,51}, {30,40,50}
 For which value of p < 1000, is the number of solutions maximised?
 '''
-# a**2 + b**2 = c**2
-
+# a**2 + b**2 = (120 - a - b)**2
+# 2a+c=p
+#
 # a + b + c = p
+# c = p - a - b
 # alist = []
-for a in range(1,49):
-    for b in range(1,21):
-        result = (a ** 2 + b ** 2) ** 0.5
-        # print(result)
-    if result.is_integer():
-        print (result)
+def triangles(p):
+    count_max = 0
+    count = 0
+    for a in range(1,int(p/2)):
+        # for b in range(int((p**2 - a**2)**0.5)-1, p-a):
+        for b in range(a, p-a):
+            # print(b)
+            if a**2 + b**2 == (p - a - b)**2:
+                count += 1
+                if count > count_max:
+                    count_max = count
+                # print(a, b)
+    return count_max
+n = []
+for i in range(500):
+    count = triangles(i)
+    n.append(count)
+
+# print(n)
+
+# print(triangles(00))
