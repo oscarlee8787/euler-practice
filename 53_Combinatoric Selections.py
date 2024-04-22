7f9a7867cb34778bfbaa6d2c9c83be1bacedf84a
Oscar Lee <oscarlee8787@gmail.com>
2024-04-22 15:06:02 +0800

    53 done fast

--- 53_Combinatoric Selections.py
+++ 53_Combinatoric Selections.py
@@ -1,20 +1,43 @@
-from itertools import combinations
+# from itertools import combinations
+
+# cnt = 0
+# for n in range(1, 101):
+#     for r in range(1, n//2 + 1):
+#         a = combinations(range(n), r)
+#         length = len([i for i in a])
+#         if length > 1000000:
+#             cnt += (n - r - r + 1)
+#             print(n, r)
+#             print(cnt)
+#             break
+# print(cnt)
+
+def fact(n, r):
+    lower = r
+    if n == 0 or n == 1:
+        return 1
+    else:
+        fact = 1
+        while lower > 0:
+            fact *= n
+            fact /= lower
+            n -= 1
+            lower -= 1
+
+        return fact
+
+# print(fact(23, 10))
 
 cnt = 0
 for n in range(1, 101):
     for r in range(1, n//2 + 1):
-        a = combinations(range(n), r)
-        length = len([i for i in a])
-        if length > 1000000:
+        if fact(n, r) > 1000000:
             cnt += (n - r - r + 1)
-            print(n, r)
-            print(cnt)
+            # print(n, r)
+            # print(cnt)
             break
 print(cnt)
 
 
-# 23 - 10 = 13
-
-# 10, 11, 12, 13
-
-# 13 - 10 + 1
+# n!
+# r! * (n-r)!
