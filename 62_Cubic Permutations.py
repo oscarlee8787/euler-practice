da956a593e5af665da7c900abad10ff809098f09
Oscar Lee <oscarlee8787@gmail.com>
2024-05-01 17:42:41 +0800

    62 done

--- 62_Cubic Permutations.py
+++ 62_Cubic Permutations.py
@@ -1,20 +1,16 @@
 cnt = dict()
+first = dict()
 
 
 for i in range(1, 50000):
-    n = ''.join(sorted(str(i**3)))
+    k = str(i**3)
+    n = ''.join(sorted(k))
     if n not in cnt:
         cnt[n] = 0
+        first[n] = i
     cnt[n] += 1
-    # if cnt[n] == 5:
-    #     print(n, i)
-for i, v in enumerate(cnt.values()):
-    if v ==5:
-        print(i, v)
 
 
-# for i in range(1, 20000):
-#     k = str(i**3)
-#     n = ''.join(sorted(k))
-#     if n == '1123445567899':
-#         print(n, i)
+ans = (first[list(cnt.keys())[list(cnt.values()).index(5)]])
+print(ans)
+print(ans**3)
