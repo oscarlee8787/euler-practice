cd5587c47069f6010e1b876ee384f590d27713ac
Oscar Lee <oscarlee8787@gmail.com>
2024-05-05 16:55:10 +0800

    p67 done

--- 67_Maximum Path Sum II.py
+++ 67_Maximum Path Sum II.py
@@ -1,15 +1,20 @@
 with open('resources/0067_triangle.txt') as file:
     text = file.read().splitlines()
-    text.reverse()
+    triangle  = []
+    for line in text:
+        k = list(map(int, line.split()))
+        triangle.append(k)
+    # print(triangle)
+    triangle.reverse()
     length = len(text)
-    num = 0
+    # print(triangle)
 
-    for idx, ele in enumerate(text):
+    for idx, ele in enumerate(triangle):
         if len(ele) == 1:
-            num += ele[0]
-            print(num)
-        for i in range(len(idx+1)):
-            num +=
+            print(ele[0])
+            break
+        for i in range(len(triangle[idx+1])):
+            triangle[idx+1][i] += max(triangle[idx][i], triangle[idx][i+1])
 
 
 
@@ -22,4 +27,4 @@ with open('resources/0067_triangle.txt') as file:
     #     new_num = max(text[i+1][left], text[i+1][right])
     #     ans += new_num
     #     pos =
-    print(text[-1])
+    # print(text[-1])
